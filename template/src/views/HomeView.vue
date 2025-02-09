b89493d836a1b4b634c791797e852ef86ed76aa7
Oseo <halicanilker@gmail.com>
2025-02-09 22:18:59 +0300

    template added

---
+++ template/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <div class="flex items-center justify-center h-screen bg-gray-100">
+      <h1 class="text-4xl font-bold text-blue-600">
+        Vue 3 + TypeScript + Tailwind CSS + Vite
+      </h1>
+    </div>
+  </main>
+</template>
